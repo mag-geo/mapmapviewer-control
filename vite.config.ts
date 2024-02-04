e50fccb4165284c10b633a1e6777cb30b633651d
xiupos <me@xiupos.net>
2024-02-04 23:31:52 +0900

    build: rename config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from "vite";
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({ configFile: '../svelte.config.js' })],
-  root: "demo",
+  plugins: [
+    svelte({ configFile: 'svelte.config.js' }),
+    dts({
+      rollupTypes: true,
+    }),
+  ],
   build: {
-    outDir: "../docs",
+    outDir: "dist",
+    emptyOutDir: false,
+    sourcemap: true,
+
+    lib: {
+      name: "mmvc",
+      entry: "src/main.ts",
+      formats: ["es", "cjs", "umd"],
+    },
   },
 });
